5d1681f526a9d1c602132811c4db19b4db84868a
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-03-19 10:59:35 +0530

    Initial Commit

---
+++ TestTest1/TestTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestTest1App.swift
+//  TestTest1
+//
+//  Created by Test Ios on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TestTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
